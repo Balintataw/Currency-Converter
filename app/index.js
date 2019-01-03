67ec019911a3367d3ce82938ae728e1b93938f82
Balintataw J <balintataw2@gmail.com>
2019-01-02 17:28:53 -0800

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const App = () => (
+  <View style={styles.container}>
+    <Text>Start writing code from app/index.js!</Text>
+  </View>
+);
+
+export default App;
+
